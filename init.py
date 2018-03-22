0b64afce4a93f81036199531beb0fb10cf9a5e43
limbo0000 <limbo0066@gmail.com>
2018-03-22 14:49:16 +0800

    add normal_init

--- init.py
+++ init.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # coding=utf-8
-import torch.nn as nn
-import nn.init.xavier_normal as xavier
-import nn.init.kaiming_normal as kaiming
-def weights_init(m):
-    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Conv3d) or isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Linear):
-        xavier(m.weight.data)
-        xavier(m.bias.data)
+import torch
+import torch.nn.init as inital
+def weights_init_normal(m):
+    if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Conv3d) or isinstance(m, torch.nn.ConvTranspose2d) or isinstance(m, torch.nn.Linear):
+        inital.normal(m.weight.data,0,0.02)
+        inital.normal(m.bias.data,0,0.02)
+def weights_init_xavier(m):
+    if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Conv3d) or isinstance(m, torch.nn.ConvTranspose2d) or isinstance(m, torch.nn.Linear):
+        inital.xavier_normal(m.weight.data)
+        inital.xavier_normal(m.bias.data)
 
 def weights_init_kaiming(m):
-    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Conv3d) or isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Linear):
-        kaiming(m.weight.data)
-        kaiming(m.bias.data)
+    if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Conv3d) or isinstance(m, torch.nn.ConvTranspose2d) or isinstance(m, torch.nn.Linear):
+        inital.kaiming_normal(m.weight.data)
+        inital.kaiming_normal(m.bias.data)
 
